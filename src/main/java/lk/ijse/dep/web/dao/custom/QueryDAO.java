15a97c985d2ea8c283ad5f6913f3f2cb2524a48c
Sajeewa Dilshan <sajeewadilshansl@gmail.com>
2021-02-01 23:03:23 +0530

    Add CustomEntity and QueryDAO

---
+++ src/main/java/lk/ijse/dep/web/dao/custom/QueryDAO.java
@@ -0,0 +1,8 @@
+package lk.ijse.dep.web.dao.custom;
+
+import lk.ijse.dep.web.dao.SuperDAO;
+
+public interface QueryDAO extends SuperDAO {
+
+
+}
---
+++ src/main/java/lk/ijse/dep/web/entity/CustomEntity.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep.web.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CustomEntity implements SuperEntity {
+
+}

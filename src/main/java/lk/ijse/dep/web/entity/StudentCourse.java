11a634ba424a601ced9d34fa16ab2048739e4be8
Sajeewa Dilshan <sajeewadilshansl@gmail.com>
2021-02-01 17:24:06 +0530

    Add StudentCourse entity

---
+++ src/main/java/lk/ijse/dep/web/entity/StudentCourse.java
@@ -0,0 +1,31 @@
+package lk.ijse.dep.web.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "student_course")
+public class StudentCourse {
+
+    @EmbeddedId
+    private StudentCoursePK studentCoursePK;
+    @Column(name = "registered_name")
+    private Date registeredDate;
+    @ManyToOne
+    @Setter(AccessLevel.NONE)
+    @JoinColumn(name = "student_id",referencedColumnName = "id",insertable = false,updatable = false)
+    private Student student;
+    @ManyToOne
+    @Setter(AccessLevel.NONE)
+    @JoinColumn(name = "course_code",referencedColumnName = "id",insertable = false,updatable = false)
+    private Course course;
+
+
+}
--- src/main/java/lk/ijse/dep/web/entity/StudentCoursePK.java
+++ src/main/java/lk/ijse/dep/web/entity/StudentCoursePK.java
@@ -4,13 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
-public class StudentCoursePK implements  {
+public class StudentCoursePK implements Serializable {
+
+    @Column(name = "student_id")
+    private String studentId;
+    @Column(name = "course_code")
+    private String courseCode;
+
 
 
 
